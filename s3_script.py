d270d382b2afcb39bfdd3f3c6a946d17847ddd23
nikhilpalkar <32640897+nikhilpalkar@users.noreply.github.com>
2023-07-26 16:23:18 +0530

    Update s3_script.py

--- s3_script.py
+++ s3_script.py
@@ -1,9 +1,9 @@
 import boto3
 import json
-access_key= 'billing_user'
-secret_key= '9zRZbj3}'
-s3_client = boto3.client('s3', aws_access_key_id=billing_user, aws_secret_access_key=9zRZbj3})
-ce_client = boto3.client('ce', aws_access_key_id=billing_user, aws_secret_access_key=9zRZbj3})
+access_key= '******'
+secret_key= '#####'
+s3_client = boto3.client('s3', aws_access_key_id=*****, aws_secret_access_key=#####)
+ce_client = boto3.client('ce', aws_access_key_id=*****, aws_secret_access_key=#####)
 def get_billing_details():
     response = ce_client.get_cost_and_usage(
         TimePeriod={'Start': '2023-07-26', 'End': '2023-08-26'},
